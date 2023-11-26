9c1d64bcd26e45e88981c6ecff952dc65d5f027c
Konstantin Filyaev <uzornakovre@yandex.ru>
2023-11-26 21:30:14 +0300

    feat: add selectors

---
+++ src/js/selectors.js
@@ -0,0 +1,16 @@
+export const content = document.querySelector(".content");
+export const rangeForm = content.querySelector(".range");
+export const rangeInput = rangeForm.querySelector(".range__input");
+export const rangeValueElement = rangeForm.querySelector(".range__value");
+export const rangeSubmitButton = rangeForm.querySelector(".range__submit");
+export const answerForm = content.querySelector(".answer");
+export const answerInput = answerForm.querySelector(".answer__input");
+export const answerSubmitButton = answerForm.querySelector(".answer__submit");
+export const outputElement = content.querySelector(".output");
+export const ouputTextElement = outputElement.querySelector(".output__text");
+export const outputTooltipElement =
+  outputElement.querySelector(".output__tooltip");
+export const outputCounterElement = outputElement.querySelector(
+  ".output__conuter-value"
+);
+export const restartButton = content.querySelector(".button_type_restart");
